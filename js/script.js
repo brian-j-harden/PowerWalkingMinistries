08ba9a55d0ef5b19317ba5969b40754b0ca03a45
Brian Harden <brian.j.harden@gmail.com>
2016-09-21 11:09:16 -0400

    Adding left nav for about us and added listeners

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function(){
     $("#header").load("templates/header.html");
     $("#menu").load("templates/menu.html", function() {
-        // When setting up the menu, put in a 100 milliscond
+        // When setting up the menu, put in a 100 millisecond
         // delay to ensure the menu loads completely before
         // adding the listeners
         setTimeout(function(){
@@ -9,7 +9,7 @@ $(function(){
         }, 100);
     });
     $("#content").load("templates/home.html");
-    $("#footer").load("footer.html");
+    $("#footer").load("templates/footer.html");
 
     // Setup menu
     function setupMenu() {
@@ -17,13 +17,30 @@ $(function(){
             $("#content").load("templates/home.html");
         });
         $("#aboutUs").on("click", function () {
-            $("#content").load("templates/about.html");
+            $("#content").load("templates/about.html", function() {
+                // When setting up the left menu, put in a 100 millisecond
+                // delay to ensure the menu loads completely before
+                // adding the listeners
+                $("#about-content").load("templates/story.html");
+                setTimeout(function(){
+                    setupAboutMenu();
+                }, 100);
+            });
         });
         $("#events").on("click", function () {
             $("#content").load("templates/events.html");
         });
         $("#give").on("click", function () {
             $("#content").load("templates/thanks.html");
+        });
+    }
+
+    function setupAboutMenu() {
+        $("#ourStory").on("click", function () {
+            $("#about-content").load("templates/story.html");
+        });
+        $("#contactUs").on("click", function () {
+            $("#about-content").load("templates/contact.html");
         });
     }
 });
